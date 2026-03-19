0cf96f8756620f6a25353eede2fcbee3bf15d9ad
Pudimlixo <viestudanteslk7@gmail.com>
2026-03-18 22:34:50 -0300

    Adição de índices e salvação aos arquivos criados com os campos mudados

--- main.py
+++ main.py
@@ -8,23 +8,41 @@
 import openpyxl # type: ignore
 from PIL import Image, ImageDraw, ImageFont # type: ignore
 
-def get_dados_planilha():
-    #Carregar qualquer planilha Excel dentro de uma variável
-    workbook_alunos = openpyxl.load_workbook('planilha_alunos.xlsx');
-    #Pegar página específica da planilha
-    pagAlunos = workbook_alunos['Sheet1'];
-    #Acessar cada linha da planilha e pegar os dados de cada coluna (Com uma iteração)
-    for linha in pagAlunos.iter_rows(min_row=2):
-        #Acessar cada célula da linha e pegar o valor
-        nomeCurso = linha[0].value
-        nomeAluno = linha[1].value
-        tipoCertificado = linha[2].value
-        dataInicio = linha[3].value
-        dataFim = linha[4].value
-        cargaHoraria = linha[5].value
-        dataEmissao = linha[6].value
+def main():
+    try:
+        # Carregar qualquer planilha Excel dentro de uma variável
+        workbook_alunos = openpyxl.load_workbook('planilha_alunos.xlsx')
+        #Pegar página específica da planilha
+        pagAlunos = workbook_alunos['Sheet1']
+        #Acessar cada linha da planilha e pegar os dados de cada coluna (Com uma iteração)
+        for index, linha in enumerate(pagAlunos.iter_rows(min_row=2), start=1):
+            #Acessar cada célula da linha e pegar o valor
+            nomeCurso = linha[0].value
+            nomeAluno = linha[1].value
+            tipoCertificado = linha[2].value
+            dataInicio = linha[3].value
+            dataFim = linha[4].value
+            cargaHoraria = linha[5].value
+            dataEmissao = linha[6].value
+    
+            #Definir fontes de cada atributo mutávelfontNome
+            fonteNome = ImageFont.truetype('./fonts/tahomabd.ttf', 90)
+            fonteGeral = ImageFont.truetype('./fonts/tahoma.ttf', 46)
+    
+    
+            if not nomeAluno:
+                continue
+        
+            image = Image.open('./certificado_padrao.jpg')
+            draw = ImageDraw.Draw(image)
+        
+            draw.text((1048,887), nomeAluno, fill="black", font=fonteNome)
+            image.save('./certificado_{}_{}.jpg'.format(index, nomeAluno))
+        
+    except FileNotFoundError as e:
+        print(f"Erro: Certifique-se de que os arquivos de imagem, fonte e planilha existem. {e}")
+    except Exception as e:
+        print(f"Ocorreu um erro inesperado: {e}")    
 
-    
-    
-    
-get_dados_planilha()
+main()
+
